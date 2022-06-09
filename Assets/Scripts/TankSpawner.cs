55a5f95cae9f543537ce8874435124ae3b8dec23
Sai Sampath Karumudi <sampath.karumudi@gmail.com>
2022-06-09 17:58:29 +0530

    Added MVC design pattern to the game

--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class TankSpawner : MonoBehaviour
 {
-    public GameObject tankPrefab;
+    public TankView tankView;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(tankPrefab, transform.position, Quaternion.identity);
+        CreateTank();
     }
 
+    private void CreateTank()
+    {
+        TankModel tankModel = new TankModel();
+        TankController tankController = new TankController(tankModel, tankView);
+    }
 }
